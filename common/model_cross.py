e2853d2b20be5dc961612fa2a1083b27ae336e0c
JamesOconer <72647348+JamesOconer@users.noreply.github.com>
2025-03-20 17:07:52 +0800

    Add files via upload

---
+++ common/model_cross.py
@@ -0,0 +1,402 @@
+## Our PoseFormer model was revised from https://github.com/rwightman/pytorch-image-models/blob/master/timm/models/vision_transformer.py
+
+import math
+import logging
+from functools import partial
+from collections import OrderedDict
+from einops import rearrange, repeat
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import time
+
+from math import sqrt
+
+from timm.data import IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD
+from timm.models.helpers import load_pretrained
+from timm.models.layers import DropPath, to_2tuple, trunc_normal_
+from timm.models.registry import register_model
+from common.rela import RectifiedLinearAttention
+from common.routing_transformer import KmeansAttention
+from common.linearattention import LinearMultiheadAttention
+
+class Mlp(nn.Module):
+    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0., changedim=False, currentdim=0, depth=0):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.fc1 = nn.Linear(in_features, hidden_features)
+        self.act = act_layer()
+        self.fc2 = nn.Linear(hidden_features, out_features)
+        self.drop = nn.Dropout(drop)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.drop(x)
+        return x
+
+
+class Attention(nn.Module):
+    def __init__(self, dim, num_heads=8, qkv_bias=False, qk_scale=None, attn_drop=0., proj_drop=0., comb=False, vis=False):
+        super().__init__()
+        self.num_heads = num_heads
+        head_dim = dim // num_heads
+        self.scale = qk_scale or head_dim ** -0.5
+        self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+        self.comb = comb
+        self.vis = vis
+
+    def forward(self, x):
+        B, N, C = x.shape
+        qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
+        q, k, v = qkv[0], qkv[1], qkv[2]
+        if self.comb==True:
+            attn = (q.transpose(-2, -1) @ k) * self.scale
+        elif self.comb==False:
+            attn = (q @ k.transpose(-2, -1)) * self.scale
+        attn = attn.softmax(dim=-1)
+        attn = self.attn_drop(attn)
+        
+        if self.comb==True:
+            x = (attn @ v.transpose(-2, -1)).transpose(-2, -1)
+            x = rearrange(x, 'B H N C -> B N (H C)')
+        elif self.comb==False:
+            x = (attn @ v).transpose(1, 2).reshape(B, N, C)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        return x
+
+class TemporalAttention(nn.Module):
+    def __init__(self, dim, num_heads=8, qkv_bias=False, qk_scale=None, attn_drop=0., proj_drop=0., comb=False, vis=False):
+        super().__init__()
+        self.num_heads = num_heads
+        head_dim = dim // num_heads
+        self.scale = qk_scale or head_dim ** -0.5
+        self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+        self.comb = comb
+        self.vis = vis
+
+    def forward(self, x, vis=False):
+        B, N, C = x.shape
+        qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
+        q, k, v = qkv[0], qkv[1], qkv[2]
+        if self.comb==True:
+            attn = (q.transpose(-2, -1) @ k) * self.scale
+        elif self.comb==False:
+            attn = (q @ k.transpose(-2, -1)) * self.scale
+        attn = attn.softmax(dim=-1)
+        attn = self.attn_drop(attn)
+        
+        if self.comb==True:
+            x = (attn @ v.transpose(-2, -1)).transpose(-2, -1)
+            x = rearrange(x, 'B H N C -> B N (H C)')
+        elif self.comb==False:
+            x = (attn @ v).transpose(1, 2).reshape(B, N, C)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        return x
+
+class ProbMask():
+    def __init__(self, B, H, L, index, scores, device="cpu"):
+        _mask = torch.ones(L, scores.shape[-1], dtype=torch.bool).to(device).triu(1)
+        _mask_ex = _mask[None, None, :].expand(B, H, L, scores.shape[-1])
+        indicator = _mask_ex[torch.arange(B)[:, None, None],
+                             torch.arange(H)[None, :, None],
+                             index, :].to(device)
+        self._mask = indicator.view(scores.shape).to(device)
+    
+    @property
+    def mask(self):
+        return self._mask
+
+class ProbAttention(nn.Module):
+    def __init__(self, dim, num_heads=8, factor=5, qkv_bias=False, qk_scale=None, attn_drop=0.1):
+        super().__init__()
+        self.factor = factor
+        self.scale = qk_scale or ((dim // num_heads) ** -0.5)
+        self.mask_flag = mask_flag
+        self.output_attention = output_attention
+        self.dropout = nn.Dropout(attn_drop)
+        self.num_heads = num_heads
+        self.mix = mix
+
+        self.query_projection = nn.Linear(dim, dim)
+        self.key_projection = nn.Linear(dim, dim)
+        self.value_projection = nn.Linear(dim, dim)
+
+        self.proj = nn.Linear(dim, dim)
+
+
+    def _prob_QK(self, Q, K, sample_k, n_top):
+        B, H, L_K, E = K.shape
+        _, _, L_Q, _ = Q.shape
+
+        # calculate the sampled Q_K
+        K_expand = K.unsqueeze(-3).expand(B, H, L_Q, L_K, E)
+        index_sample = torch.randint(L_K, (L_Q, sample_k)) # real U = U_part(factor*ln(L_k))*L_q
+        K_sample = K_expand[:, :, torch.arange(L_Q).unsqueeze(1), index_sample, :]
+        Q_K_sample = torch.matmul(Q.unsqueeze(-2), K_sample.transpose(-2, -1)).squeeze()
+
+        M = Q_K_sample.max(-1)[0] - torch.div(Q_K_sample.sum(-1), L_K)
+        M_top = M.topk(n_top, sorted=False)[1]
+
+        Q_reduce = Q[torch.arange(B)[:, None, None],
+                     torch.arange(H)[None, :, None],
+                     M_top, :]
+        Q_K = torch.matmul(Q_reduce, K.transpose(-2, -1))
+
+        return Q_K, M_top
+
+    def _get_initial_context(self, V, L_Q):
+        B, H, L_V, D = V.shape
+        if not self.mask_flag:
+            V_sum = V.mean(dim=-2)
+            contex = V_sum.unsqueeze(-2).expand(B, H, L_Q, V_sum.shape[-1]).clone()
+        else:
+            assert(L_Q == L_V)
+            contex = V.cumsum(dim=-2)
+        return contex
+
+    def _update_context(self, context_in, V, scores, index, L_Q, attn_mask):
+        B, H, L_V, D = V.shape
+
+        if self.mask_flag:
+            attn_mask = ProbMask(B, H, L_Q, index, scores, device=V.device)
+            scores.masked_fill_(attn_mask.mask, -np.inf)
+
+        attn = torch.softmax(scores, dim=-1) # nn.Softmax(dim=-1)(scores)
+
+        context_in[torch.arange(B)[:, None, None],
+                   torch.arange(H)[None, :, None],
+                   index, :] = torch.matmul(attn, V).type_as(context_in)
+        if self.output_attention:
+            attns = (torch.ones([B, H, L_V, L_V])/L_V).type_as(attn).to(attn.device)
+            attns[torch.arange(B)[:, None, None], torch.arange(H)[None, :, None], index, :] = attn
+            return (context_in, attns)
+        else:
+            return (context_in, None)
+
+    def forward(self, x, attn_mask=None):
+        B, N, C = x.shape
+        queries = self.query_projection(x).view(B, N, self.num_heads, -1)
+        keys = self.key_projection(x).view(B, N, self.num_heads, -1)
+        values = self.value_projection(x).view(B, N, self.num_heads, -1)
+        B, L_Q, H, D = queries.shape
+        _, L_K, _, _ = keys.shape
+
+        queries = queries.transpose(2,1)
+        keys = keys.transpose(2,1)
+        values = values.transpose(2,1)
+
+        U_part = self.factor * np.ceil(np.log(L_K)).astype('int').item() # c*ln(L_k)
+        u = self.factor * np.ceil(np.log(L_Q)).astype('int').item() # c*ln(L_q)
+        U_part = U_part if U_part<L_K else L_K
+        u = u if u<L_Q else L_Q
+        
+        scores_top, index = self._prob_QK(queries, keys, sample_k=U_part, n_top=u)
+        scale = self.scale or 1./sqrt(D)
+        scores_top = scores_top * scale
+        context = self._get_initial_context(values, L_Q)
+        context, attn = self._update_context(context, values, scores_top, index, L_Q, attn_mask)
+        context = context.transpose(2,1).contiguous()
+        if self.mix:
+            context = context.transpose(2,1).contiguous()
+        
+        context = context.reshape(B, N, -1)
+        context = self.proj(context)
+        return context
+
+
+class Block(nn.Module):
+
+    def __init__(self, dim, num_heads, mlp_ratio=4., attention=Attention, qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
+                 drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm, comb=False, changedim=False, currentdim=0, depth=0, vis=False):
+        super().__init__()
+
+        self.changedim = changedim
+        self.currentdim = currentdim
+        self.depth = depth
+        if self.changedim:
+            assert self.depth>0
+
+        self.norm1 = norm_layer(dim)
+        self.attn = attention(
+            dim, num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale, attn_drop=attn_drop, proj_drop=drop, comb=comb, vis=vis)
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+        self.norm2 = norm_layer(dim)
+        mlp_hidden_dim = int(dim * mlp_ratio)
+        self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
+        
+        if self.changedim and self.currentdim < self.depth//2:
+            self.reduction = nn.Conv1d(dim, dim//2, kernel_size=1)
+        elif self.changedim and depth > self.currentdim > self.depth//2:
+            self.improve = nn.Conv1d(dim, dim*2, kernel_size=1)
+        self.vis = vis
+
+    def forward(self, x, vis=False):
+        x = x + self.drop_path(self.attn(self.norm1(x), vis=vis))
+        x = x + self.drop_path(self.mlp(self.norm2(x)))
+        
+        if self.changedim and self.currentdim < self.depth//2:
+            x = rearrange(x, 'b t c -> b c t')
+            x = self.reduction(x)
+            x = rearrange(x, 'b c t -> b t c')
+        elif self.changedim and self.depth > self.currentdim > self.depth//2:
+            x = rearrange(x, 'b t c -> b c t')
+            x = self.improve(x)
+            x = rearrange(x, 'b c t -> b t c')
+        return x
+
+class TemporalBlock(nn.Module):
+    
+    def __init__(self, dim, num_heads, mlp_ratio=4., attention=Attention, qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
+                 drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm, comb=False, changedim=False, currentdim=0, depth=0, vis=False):
+        super().__init__()
+
+        self.changedim = changedim
+        self.currentdim = currentdim
+        self.depth = depth
+        if self.changedim:
+            assert self.depth>0
+
+        self.norm1 = norm_layer(dim)
+        self.attn = attention(
+            dim, num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale, attn_drop=attn_drop, proj_drop=drop, comb=comb, vis=vis)
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+        self.norm2 = norm_layer(dim)
+        mlp_hidden_dim = int(dim * mlp_ratio)
+        self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
+        
+        if self.changedim and self.currentdim < self.depth//2:
+            self.reduction = nn.Conv1d(dim, dim//2, kernel_size=1)
+        elif self.changedim and depth > self.currentdim > self.depth//2:
+            self.improve = nn.Conv1d(dim, dim*2, kernel_size=1)
+        self.vis = vis
+        self.reduction = nn.Linear(dim, dim//2)
+
+    def forward(self, x, vis=False):
+        x = x + self.drop_path(self.attn(self.norm1(x), vis=vis))
+        x = x + self.drop_path(self.mlp(self.norm2(x)))
+        x = self.reduction(x)
+        return x
+
+class attn_pooling(nn.Module):
+    def __init__(self, in_feature, out_feature, stride=2, padding_mode='zeros'):
+        super(attn_pooling, self).__init__()
+
+        self.conv = nn.Conv1d(in_feature, out_feature, kernel_size=stride + 1,
+                              padding=stride // 2, stride=stride,
+                              padding_mode=padding_mode, groups=in_feature)
+        self.fc = nn.Linear(in_feature, out_feature)
+
+    def forward(self, x):
+
+        x = self.conv(x)
+
+        return x
+
+class  MCLICE(nn.Module):
+    def __init__(self, num_frame=9, num_joints=17, in_chans=2, embed_dim_ratio=32, depth=4,
+                 num_heads=8, mlp_ratio=2., qkv_bias=True, qk_scale=None,
+                 drop_rate=0., attn_drop_rate=0., drop_path_rate=0.2,  norm_layer=None):
+        super().__init__()
+
+        norm_layer = norm_layer or partial(nn.LayerNorm, eps=1e-6)
+        embed_dim = embed_dim_ratio
+        out_dim = 3
+        self.Spatial_patch_to_embedding = nn.Linear(in_chans, embed_dim_ratio)
+        self.Spatial_pos_embed = nn.Parameter(torch.zeros(1, num_joints, embed_dim_ratio))
+        self.Temporal_pos_embed = nn.Parameter(torch.zeros(1, num_frame, embed_dim))
+        self.pos_drop = nn.Dropout(p=drop_rate)
+
+
+        dpr = [x.item() for x in torch.linspace(0, drop_path_rate, depth)]
+        self.block_depth = depth
+
+        self.STEblocks = nn.ModuleList([
+            Block(
+                dim=embed_dim_ratio, num_heads=num_heads, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,
+                drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[i], norm_layer=norm_layer)
+            for i in range(depth)])
+
+        self.TTEblocks = nn.ModuleList([
+            Block(
+                dim=embed_dim, num_heads=num_heads, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,
+                drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[i], norm_layer=norm_layer, comb=False, changedim=False, currentdim=i+1, depth=depth)
+            for i in range(depth)])
+
+        self.Spatial_norm = norm_layer(embed_dim_ratio)
+        self.Temporal_norm = norm_layer(embed_dim)
+        self.head = nn.Sequential(
+            nn.LayerNorm(embed_dim),
+            nn.Linear(embed_dim , out_dim),
+        )
+
+    def STE_forward(self, x):
+        b, f, n, c = x.shape
+        x = rearrange(x, 'b f n c  -> (b f) n c', )
+        x = self.Spatial_patch_to_embedding(x)
+
+        x += self.Spatial_pos_embed
+        x = self.pos_drop(x)
+        blk = self.STEblocks[0]
+        x = blk(x)
+        x = self.Spatial_norm(x)
+        x = rearrange(x, '(b f) n cw -> (b n) f cw', f=f)
+        return x
+
+    def TTE_foward(self, x):
+        assert len(x.shape) == 3, "shape is equal to 3"
+        b, f, _  = x.shape
+        x += self.Temporal_pos_embed
+        x = self.pos_drop(x)
+        blk = self.TTEblocks[0]
+        x = blk(x)
+        x = self.Temporal_norm(x)
+        return x
+
+    def ST_foward(self, x):
+        assert len(x.shape)==4, "shape is equal to 4"
+        b, f, n, cw = x.shape
+        for i in range(1, self.block_depth):
+            x = rearrange(x, 'b f n cw -> (b f) n cw')
+            steblock = self.STEblocks[i]
+            tteblock = self.TTEblocks[i]
+            x = steblock(x)
+            x = self.Spatial_norm(x)
+            x = rearrange(x, '(b f) n cw -> (b n) f cw', f=f)
+            x = tteblock(x)
+            x = self.Temporal_norm(x)
+            x = rearrange(x, '(b n) f cw -> b f n cw', n=n)
+
+        return x
+
+    def forward(self, x):
+        b, f, n, c = x.shape
+        x = self.STE_forward(x)
+        x = self.TTE_foward(x)
+        x = rearrange(x, '(b n) f cw -> b f n cw', n=n)
+        x = self.ST_foward(x)
+        x = self.head(x)
+        x = x.view(b, f, n, -1)
+
+        return x
+
+    
+
+
+
+
+
